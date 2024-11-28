4e34bf922b7650b11129ed910de7f481454d2097
febas <febas@192.168.1.3>
2024-11-28 10:35:39 +0530

    Sample commit

---
+++ mevanProject_1/src/main/java/mevanPackage/InstanceExp_1.java
@@ -0,0 +1,21 @@
+package mevanPackage;
+
+public class InstanceExp_1 {
+	public void add()
+	{
+	int a=5;
+	int b=6;
+	int c=a+b;
+	System.out.println(c);
+	}
+	public static void main(String[] args)
+	{
+
+		InstanceExp_1 obj=new InstanceExp_1();
+		obj. add();
+		
+
+	}
+	 
+
+}
